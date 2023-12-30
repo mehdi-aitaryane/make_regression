01c0a286aeda40ee2edf481f4b1a39a609093421
Mehdi AIT ARYANE <mehdi.aitaryane@outlook.com>
2023-12-30 01:43:21 +0100

    update plots size

--- plots.py
+++ plots.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 # y: a 1D array of targets
 
 def plot2D(title, xlabel, ylabel, X, y):
+    plt.figure(figsize=(20, 8))
     plt.plot(X[:, 0], y)
     plt.title(title)
     plt.xlabel(xlabel)
@@ -23,13 +24,14 @@ def plot2D(title, xlabel, ylabel, X, y):
 # y: a 1D array of targets
 
 def plot3D(title, xlabel, ylabel, zlabel, X, y):
-    # Creating plot
-    ax = plt.axes(projection ="3d")
+    # Create a new figure
+    fig = plt.figure(figsize=(28, 13))
+    # Add a subplot with 3D projection
+    ax = fig.add_subplot(111, projection='3d')
+    # Create the 3D plot
     ax.plot3D(X[:, 0], X[:, 1], y)
     plt.title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_zlabel(zlabel)
-    # Rotate the plot (change the angles as needed)
-    ax.view_init(elev=30 , azim=-80 )
     plt.show()
